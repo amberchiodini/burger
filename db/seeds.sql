c765d56c5dbdc5c4659aff5ba6d94ea1338ce38f
amberchiodini <69092983+amberchiodini@users.noreply.github.com>
2021-01-10 19:59:18 -0600

    Initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Kimchi Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheesburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheesburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
